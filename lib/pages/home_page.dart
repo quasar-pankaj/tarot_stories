9ce58a4ee870125557ff2c8646b9c082dcf96630
Pankaj Agarwal <quasar.pankaj@gmail.com>
2022-12-08 16:16:40 +0530

    fixed a problem with project page.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tarot_stories/providers/open_project_provider.dart';
 
 import '../database/entities/project.dart';
 import '../providers/projects_in_memory_notifier_provider.dart';
@@ -19,11 +20,12 @@ class HomePage extends ConsumerWidget {
       body: Column(
         children: [
           Toolbar(
-              sortOrderButtonsProvider: projectSortOrderButtonsProvider,
-              sortOrderProvider: projectSortOrderProvider,
-              sortConditionButtonsProvider: projectSortConditionButtonsProvider,
-              sortConditionProvider: projectSortConditionProvider,
-              filterTextProvider: projectFilterTextProvider),
+            sortOrderButtonsProvider: projectSortOrderButtonsProvider,
+            sortOrderProvider: projectSortOrderProvider,
+            sortConditionButtonsProvider: projectSortConditionButtonsProvider,
+            sortConditionProvider: projectSortConditionProvider,
+            filterTextProvider: projectFilterTextProvider,
+          ),
           Consumer(
             builder: (context, ref, child) {
               final result = ref.watch(sortedFilteredProjectListProvider);
@@ -35,8 +37,8 @@ class HomePage extends ConsumerWidget {
                   childAspectRatio: 0.75,
                   children: data
                       .map(
-                        (e) => Dismissible(
-                          key: Key(e.toString()),
+                        (project) => Dismissible(
+                          key: Key(project.toString()),
                           child: Card(
                             color: Colors.green,
                             child: InkWell(
@@ -44,7 +46,12 @@ class HomePage extends ConsumerWidget {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (context) => const ProjectPage(),
+                                    builder: (context) {
+                                      ref
+                                          .read(openProjectProvider.notifier)
+                                          .update((state) => project);
+                                      return const ProjectPage();
+                                    },
                                   ),
                                 );
                               },
@@ -53,9 +60,10 @@ class HomePage extends ConsumerWidget {
                                   child: Padding(
                                     padding: const EdgeInsets.all(8.0),
                                     child: InPlaceEditor(
-                                      text: e.name,
+                                      text: project.name,
                                       onTextChanged: (newText) {
-                                        final Project project = e.copyWith(
+                                        final Project renamedProject =
+                                            project.copyWith(
                                           name: newText,
                                           modifiedTimestamp: DateTime.now()
                                               .millisecondsSinceEpoch,
@@ -63,7 +71,7 @@ class HomePage extends ConsumerWidget {
                                         ref
                                             .read(inMemoryProjectsProvider
                                                 .notifier)
-                                            .update(project);
+                                            .update(renamedProject);
                                       },
                                     ),
                                   ),
@@ -78,7 +86,7 @@ class HomePage extends ConsumerWidget {
                           ),
                           onDismissed: (direction) => ref
                               .read(inMemoryProjectsProvider.notifier)
-                              .remove(e),
+                              .remove(project),
                         ),
                       )
                       .toList(),
