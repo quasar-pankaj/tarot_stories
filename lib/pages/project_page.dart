c87f836f656f45771014aad4e2da15e4cf9e2eae
Pankaj Agarwal <quasar.pankaj@gmail.com>
2023-01-03 14:37:01 +0530

    journal widget

--- lib/pages/project_page.dart
+++ lib/pages/project_page.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../database/entities/journal.dart';
 import '../providers/project/open_project_provider.dart';
-import '../providers/journals/open_journal_provider.dart';
-import '../providers/journals/sorted_filtered_journal_list_provider.dart';
-import '../providers/journals/journal_filter_text_provider.dart';
-import '../providers/journals/journal_sort_condition_buttons_provider.dart';
-import '../providers/journals/journal_sort_condition_provider.dart';
-import '../providers/journals/journal_sort_order_buttons_provider.dart';
-import '../providers/journals/journal_sort_order_provider.dart';
-import '../providers/journals/journals_provider.dart';
 import '../widgets/elements_sidebar.dart';
-import '../widgets/in_place_editor.dart';
-import '../widgets/toolbar.dart';
+import '../widgets/journal_widget.dart';
 
 class ProjectPage extends StatelessWidget {
   const ProjectPage({super.key});
@@ -29,116 +19,18 @@ class ProjectPage extends StatelessWidget {
       ),
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Expanded(
+        children: const [
+          Expanded(
             flex: 1,
             child: ElementsSidebar(),
           ),
-          const VerticalDivider(
+          VerticalDivider(
             width: 2,
             thickness: 2,
           ),
           Expanded(
             flex: 3,
-            child: Column(
-              children: [
-                Consumer(
-                  builder:
-                      (BuildContext context, WidgetRef ref, Widget? child) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Toolbar(
-                        sortOrderButtonsProvider:
-                            journalSortOrderButtonsProvider,
-                        sortOrderProvider: journalSortOrderProvider,
-                        sortConditionButtonsProvider:
-                            journalSortConditionButtonsProvider,
-                        sortConditionProvider: journalSortConditionProvider,
-                        filterTextProvider: journalFilterTextProvider,
-                      ),
-                    );
-                  },
-                ),
-                Consumer(
-                  builder: (context, ref, child) {
-                    final result = ref.watch(sortedFilteredJournalListProvider);
-
-                    return result.when(
-                      data: (data) {
-                        return GridView.extent(
-                          maxCrossAxisExtent: 150,
-                          childAspectRatio: 0.75,
-                          shrinkWrap: true,
-                          children: data
-                              .map(
-                                (journal) => Dismissible(
-                                  key: Key(journal.toString()),
-                                  child: Card(
-                                    color: Colors.green,
-                                    child: InkWell(
-                                      onTap: () {
-                                        ref
-                                            .read(openJournalProvider.notifier)
-                                            .update((state) => journal);
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) {
-                                              return const ProjectPage();
-                                            },
-                                          ),
-                                        );
-                                      },
-                                      child: GridTile(
-                                        header: Center(
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: InPlaceEditor(
-                                              text: journal.name,
-                                              onTextChanged: (newText) {
-                                                final Journal renamedSpread =
-                                                    journal.copyWith(
-                                                  name: newText,
-                                                  modifiedTimestamp: DateTime
-                                                          .now()
-                                                      .millisecondsSinceEpoch,
-                                                );
-                                                ref
-                                                    .read(journalProvider(
-                                                            renamedSpread
-                                                                .elementId)
-                                                        .notifier)
-                                                    .save(renamedSpread);
-                                              },
-                                            ),
-                                          ),
-                                        ),
-                                        child: const Icon(
-                                          Icons.analytics_outlined,
-                                          size: 80.6,
-                                          color: Colors.yellowAccent,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  onDismissed: (direction) => ref
-                                      .read(
-                                          journalProvider(journal.id!).notifier)
-                                      .delete(journal),
-                                ),
-                              )
-                              .toList(),
-                        );
-                      },
-                      error: (error, stackTrace) => Text(
-                        error.toString(),
-                      ),
-                      loading: () => const CircularProgressIndicator(),
-                    );
-                  },
-                ),
-              ],
-            ),
+            child: JournalWidget(),
           ),
         ],
       ),
---
+++ lib/widgets/journal_widget.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../database/entities/journal.dart';
+import '../pages/reading_page.dart';
+import '../providers/journals/journal_filter_text_provider.dart';
+import '../providers/journals/journal_sort_condition_buttons_provider.dart';
+import '../providers/journals/journal_sort_condition_provider.dart';
+import '../providers/journals/journal_sort_order_buttons_provider.dart';
+import '../providers/journals/journal_sort_order_provider.dart';
+import '../providers/journals/journals_provider.dart';
+import '../providers/journals/open_journal_provider.dart';
+import '../providers/journals/sorted_filtered_journal_list_provider.dart';
+import 'in_place_editor.dart';
+import 'toolbar.dart';
+
+class JournalWidget extends StatelessWidget {
+  const JournalWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Consumer(
+          builder: (BuildContext context, WidgetRef ref, Widget? child) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Toolbar(
+                sortOrderButtonsProvider: journalSortOrderButtonsProvider,
+                sortOrderProvider: journalSortOrderProvider,
+                sortConditionButtonsProvider:
+                    journalSortConditionButtonsProvider,
+                sortConditionProvider: journalSortConditionProvider,
+                filterTextProvider: journalFilterTextProvider,
+              ),
+            );
+          },
+        ),
+        Consumer(
+          builder: (context, ref, child) {
+            final result = ref.watch(sortedFilteredJournalListProvider);
+
+            return result.when(
+              data: (data) {
+                return GridView.extent(
+                  maxCrossAxisExtent: 150,
+                  childAspectRatio: 0.75,
+                  shrinkWrap: true,
+                  children: data
+                      .map(
+                        (journal) => Dismissible(
+                          key: Key(journal.toString()),
+                          child: Card(
+                            color: Colors.green,
+                            child: InkWell(
+                              onTap: () {
+                                ref
+                                    .read(openJournalProvider.notifier)
+                                    .update((state) => journal);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return const ReadingPage(
+                                        journal: FlutterLogo(),
+                                        spread: FlutterLogo(),
+                                      );
+                                    },
+                                  ),
+                                );
+                              },
+                              child: GridTile(
+                                header: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: InPlaceEditor(
+                                      text: journal.name,
+                                      onTextChanged: (newText) {
+                                        final Journal renamedSpread =
+                                            journal.copyWith(
+                                          name: newText,
+                                          modifiedTimestamp: DateTime.now()
+                                              .millisecondsSinceEpoch,
+                                        );
+                                        ref
+                                            .read(journalProvider(
+                                                    renamedSpread.elementId)
+                                                .notifier)
+                                            .save(renamedSpread);
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.analytics_outlined,
+                                  size: 80.6,
+                                  color: Colors.yellowAccent,
+                                ),
+                              ),
+                            ),
+                          ),
+                          onDismissed: (direction) => ref
+                              .read(journalProvider(journal.id!).notifier)
+                              .delete(journal),
+                        ),
+                      )
+                      .toList(),
+                );
+              },
+              error: (error, stackTrace) => Text(
+                error.toString(),
+              ),
+              loading: () => const CircularProgressIndicator(),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
