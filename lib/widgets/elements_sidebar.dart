8245284e23d4025f4597876406a32de94f3662c9
Pankaj Agarwal <quasar.pankaj@gmail.com>
2023-01-04 15:28:58 +0530

    changed color

--- lib/widgets/elements_sidebar.dart
+++ lib/widgets/elements_sidebar.dart
@@ -51,7 +51,7 @@ class ElementsSidebar extends StatelessWidget {
                         child: Card(
                           child: ListTile(
                             selectedColor: Colors.blue[400],
-                            selectedTileColor: Colors.pink[50],
+                            selectedTileColor: Colors.amber[50],
                             selected: selectedElement == element,
                             title: InPlaceEditor(
                               text: element.name,
